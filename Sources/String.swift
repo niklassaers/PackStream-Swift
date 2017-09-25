c581ac1752851e07792176f29b18747f97c05824
Niklas Saers <niklas@saers.com>
2017-09-25 22:47:51 +0200

    Compare on UInt64 instead of UInt
    error: integer literal '4294967295' overflows when stored into 'Int'

--- Sources/String.swift
+++ Sources/String.swift
@@ -25,7 +25,7 @@ extension String: PackProtocol {
             }
         }
 
-        let n = UInt(bytes.count)
+        let n = UInt64(bytes.count)
 
         if n == 0 {
             return [ 0x80 ]
